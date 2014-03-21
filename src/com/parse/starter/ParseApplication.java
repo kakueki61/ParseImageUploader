2598981032cddce4b5421479a1c9921b3c1818a1
kakueki61 <t.kodama61@gmail.com>
2014-03-21 21:32:47 +0900

    initial commit including Parse libs and sample codes

---
+++ src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,29 @@
+package com.parse.starter;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+import com.parse.ParseUser;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Add your initialization code here
+        Parse.initialize(this, "8rJMlGA4vE4Tamybldox5VbbdKZwyeJMo5AqGQUH", "LVdhk1wBHSNuIb2ExvrXiPC9NqykIuCC0ZbIneYT");
+
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this line.
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+
+}
