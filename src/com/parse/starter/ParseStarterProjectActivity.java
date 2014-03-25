1d6f55d68470259b4230023dd199b2f51c593f96
kakueki61 <t.kodama61@gmail.com>
2014-03-26 03:35:05 +0900

    get an image from Gallery apps and upload it

--- src/com/parse/starter/ParseStarterProjectActivity.java
+++ src/com/parse/starter/ParseStarterProjectActivity.java
@@ -1,9 +1,15 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +18,10 @@ import com.parse.*;
 import org.kakueki61.image_uploader.listener.IEditTextListener;
 import org.kakueki61.image_uploader.utils.DialogUtils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class ParseStarterProjectActivity extends Activity {
@@ -68,8 +78,64 @@ public class ParseStarterProjectActivity extends Activity {
         findViewById(R.id.button_upload_images).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), MediaImageGalleryActivity.class);
+//                startActivity(intent);
 
+                /*
+                Cursor cursor = managedQuery(
+                        MediaStore.Images.Media.INTERNAL_CONTENT_URI, null, null, null, null
+                );
+                cursor.moveToFirst();
+
+                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                Long id = cursor.getLong(fieldIndex);
+                Uri bmpUri = ContentUris.withAppendedId(MediaStore.Images.Media.INTERNAL_CONTENT_URI, id);
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), bmpUri);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    byte[] bytes = baos.toByteArray();
+
+                    ParseFile parseFile = new ParseFile("test_image.jpg", bytes);
+                    parseFile.saveInBackground();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                */
+
+                // Try to call Gallery
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1111);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 1111 && resultCode == RESULT_OK) {
+            try {
+                InputStream is = getContentResolver().openInputStream(data.getData());
+                Bitmap bitmap = BitmapFactory.decodeStream(is);
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] bytes = baos.toByteArray();
+
+                ParseFile parseFile = new ParseFile("test_image.jpg", bytes);
+
+                Log.d(TAG, "upload an image");
+                parseFile.saveInBackground();
+                Log.d(TAG, "finish to upload");
+
+                ParseObject imageParseObject = new ParseObject("ImageObject");
+                imageParseObject.put("img_binary", parseFile);
+                imageParseObject.saveInBackground();
+                Log.d(TAG, "finish");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
