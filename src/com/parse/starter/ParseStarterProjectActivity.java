895edc6d6409d7b65a946ed3bdf05a29c1241412
kakueki61 <t.kodama61@gmail.com>
2014-03-26 01:19:16 +0900

    [sample code] retrieve data from cloud

--- src/com/parse/starter/ParseStarterProjectActivity.java
+++ src/com/parse/starter/ParseStarterProjectActivity.java
@@ -8,16 +8,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
-import com.parse.Parse;
-import com.parse.ParseAnalytics;
-import com.parse.ParseObject;
+import com.parse.*;
 import org.kakueki61.image_uploader.listener.IEditTextListener;
 import org.kakueki61.image_uploader.utils.DialogUtils;
 
+import java.util.List;
+
 public class ParseStarterProjectActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+    private static final String TAG = ParseStarterProjectActivity.class.getSimpleName();
+    private static final String PARSE_OBJECT_NAME = "EditText";
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
@@ -34,7 +34,7 @@ public class ParseStarterProjectActivity extends Activity {
                 LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 final View dialogView = inflater.inflate(R.layout.dialog_input_text, null);
 
-                final ParseObject editTextObject = new ParseObject("EditText");
+                final ParseObject editTextObject = new ParseObject(PARSE_OBJECT_NAME);
                 DialogUtils.showLayoutDialog(ParseStarterProjectActivity.this, dialogView, new IEditTextListener() {
                     @Override
                     public void onIputFinish(View view) {
@@ -42,7 +42,7 @@ public class ParseStarterProjectActivity extends Activity {
                         String input = editText.getText().toString();
                         editTextObject.put("input", input);
                         editTextObject.saveInBackground();
-                        Log.d("ImageUploader", input);
+                        Log.d(TAG, input);
                     }
                 });
             }
@@ -50,7 +50,17 @@ public class ParseStarterProjectActivity extends Activity {
         findViewById(R.id.button_fetch).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery(PARSE_OBJECT_NAME);
+                parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> parseObjects, ParseException e) {
+                        for(int i = 0; i < parseObjects.size(); i++) {
+                            Log.d(TAG, parseObjects.get(i).getObjectId());
+                            Log.d(TAG, parseObjects.get(i).getCreatedAt().toString());
+                            Log.d(TAG, parseObjects.get(i).getString("input"));
+                        }
+                    }
+                });
             }
         });
     }
